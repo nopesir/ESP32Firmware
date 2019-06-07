68834236a1d0e7edb9c756f2aa8514169e647729
Luigi Ferrettino <s254300@studenti.polito.it>
2019-06-07 17:09:03 +0200

    Added "name" in the state, unified the subscription adjusted the code

--- fs/init.js
+++ fs/init.js
@@ -32,6 +32,7 @@ let data = {
 };
 let state = {
   id: "esp32",
+  name: "",
   warm: false,
   cool: false,
   enabled: false,
@@ -41,21 +42,6 @@ let state = {
   desiredTemp: 15,
   timestamp: 0
 };
-let online = false;
-
-let myDHT = DHT.create(tempPin, DHT.DHT22);
-
-state.id = Cfg.get('device.id');
-ssid = Cfg.get('wifi.sta.ssid');
-
-GPIO.set_mode(2, GPIO.MODE_OUTPUT);
-
-//{HI} =c_{1}+c_{2}T+c_{3}R+c_{4}TR+c_{5}T^{2}+c_{6}R^{2}+c_{7}T^{2}R+c_{8}TR^{2}+c_{9}T^{2}R^{2}}
-
-let state_topic = state.id + '/event/state';
-let settemp_topic = state.id + '/event/setTemp';
-let onoff_topic = state.id + '/event/onoff';
-let status_topic = state.id + '/event/status';
 
 if (Cfg.get('wifi.ap.enable')) {
   GPIO.blink(2, 250, 250);
@@ -63,12 +49,31 @@ if (Cfg.get('wifi.ap.enable')) {
   GPIO.blink(2, 0, 0)
 }
 
+state.id = "Mongoose_" + ("" + Cfg.get('device.id')).slice(6, 12);
+
+GPIO.set_mode(2, GPIO.MODE_OUTPUT);
+GPIO.set_mode(coolPin, GPIO.MODE_OUTPUT);
+GPIO.set_mode(warmPin, GPIO.MODE_OUTPUT);
+
+let online = false;
+let myDHT = DHT.create(tempPin, DHT.DHT22);
+let state_topic = state.id + '/event/state';
+let settemp_topic = state.id + '/event/setTemp';
+let onoff_topic = state.id + '/event/onoff';
+let status_topic = state.id + '/event/status';
+let setname_topic = state.id + '/event/setname';
+let general_topic = state.id + '/event/+';
+
+
 
 RPC.call(RPC.LOCAL, 'Config.Set', { config: { mqtt: { will_topic: status_topic, keep_alive: 30 } } }, function (resp, ud) {
   RPC.call(RPC.LOCAL, 'Config.Save', { reboot: false }, function (resp, ud) {
     print('Response:', JSON.stringify(resp));
   }, null);
 }, null);
+
+
+
 
 GPIO.set_button_handler(0, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function (x) {
   RPC.call(RPC.LOCAL, 'Config.Set', data, function (resp, ud) {
@@ -78,12 +83,7 @@ GPIO.set_button_handler(0, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function (x) {
   }, null);
 }, null);
 
-Event.addHandler(Net.STATUS_CONNECTED, function (ev, evdata, ud) {
-  //mdns_init();
 
-}, null);
-
-//mos config-set wifi.sta.ssid=TISCALI-C9F405 wifi.sta.pass=C62AA7FC2C wifi.sta.enable=true wifi.ap.enable=false
 
 let coolSW = function (cool) {
   let level = cool ? 0 : 1;
@@ -95,9 +95,6 @@ let warmSW = function (warm) {
   GPIO.write(warmPin, level);
 };
 
-GPIO.set_mode(coolPin, GPIO.MODE_OUTPUT);
-GPIO.set_mode(warmPin, GPIO.MODE_OUTPUT);
-
 
 coolSW(state.cool);
 warmSW(state.warm);
@@ -105,16 +102,11 @@ GPIO.write(2, 0);
 
 function updateSW() {
 
-  // HERE: get temp and save in currTemp
-
   state.cool = false;
   state.warm = false;
 
   state.currTemp = myDHT.getTemp();
   state.humidity = myDHT.getHumidity();
-  //state.currTemp = ESP32.temp();
- 
-  //state.humidity = ESP32.hall();
 
   if ((state.humidity > 40) && (state.currTemp > 27)) {
     state.percTemp = -8.78469475556 + (1.61139411 * state.currTemp) + (2.33854883889 * state.humidity) +
@@ -151,30 +143,31 @@ Timer.set(10000, Timer.REPEAT, function () {
 }, null);
 
 
-MQTT.sub(settemp_topic, function (conn, topic, msg) {
-  let temp = JSON.parse(msg);
-  if ((temp > -11) && (temp < 46)) {
-    state.desiredTemp = temp;
-    updateSW();
+MQTT.sub(general_topic, function (conn, topic, msg) {
+  if (("" + topic) === ("" + settemp_topic)) {
+    let temp = JSON.parse(msg);
+    if ((temp > -11) && (temp < 46)) {
+      state.desiredTemp = temp;
+    }
   }
-});
+  else if (("" + topic) === ("" + onoff_topic)) {
+    if (msg === "on") {
+      state.enabled = true;
+    }
+    if (msg === "off") {
+      state.enabled = false;
+    }
+  }
+  else if (("" + topic) === ("" + setname_topic)) {
+    state.name = "" + msg;
+  }
+  else if (("" + topic) !== ("" + state_topic)) {
+    print("" + topic + " not vaid!");
+  }
 
-MQTT.sub(onoff_topic, function (conn, topic, msg) {
-  if (msg === "on") {
-    state.enabled = true;
-    updateSW();
-  }
-  if (msg === "off") {
-    state.enabled = false;
-    updateSW()
-  }
-});
-
-// Only for DEBUG************************************/
-/*MQTT.sub("event/getTemp", function(conn, topic, msg) {
-  state.currTemp = JSON.parse(msg);
   updateSW();
-});*/
+});
+
 
 MQTT.setEventHandler(function (conn, ev, edata) {
   if (ev === 202) {
@@ -187,34 +180,7 @@ MQTT.setEventHandler(function (conn, ev, edata) {
   else if (ev === 5) {
     GPIO.write(2, 0);
     online = false;
+    state.enabled = false;
+    updateSW();
   }
 }, null);
-
-//***************************************************/
-
-// Set up Shadow handler to synchronise device state with the shadow state
-/*Shadow.addHandler(function (event, obj) {
-  if (event === 'UPDATE_DELTA') {
-    print('GOT DELTA:', JSON.stringify(obj));
-    for (let key in obj) {  // Iterate over all keys in delta
-      if (key === 'cool') {  // We know about the 'on' key. Handle it!
-        state.cool = obj.cool;  // Synchronise the state
-        coolSW(state.cool);   // according to the delta
-      }
-      if (key === 'warm') {  // We know about the 'on' key. Handle it!
-        state.warm = obj.warm;  // Synchronise the state
-        warmSW(state.warm);   // according to the delta
-      }
-    }
-    reportState();  // Report our new state, hopefully clearing delta
-  }
-});
-
-Event.on(Event.CLOUD_CONNECTED, function () {
-  online = true;
-  Shadow.update(0, { ram_total: Sys.total_ram() });
-}, null);
-
-Event.on(Event.CLOUD_DISCONNECTED, function () {
-  online = false;
-}, null);*/
